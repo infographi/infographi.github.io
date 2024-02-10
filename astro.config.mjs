b677d10b25902516337d90fb8e40832ac8d4c859
infographi <purser.profuse0k@icloud.com>
2024-02-10 16:30:31 +0100

    Moving Files...

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://infographi.github.io',
+	base: '/infographi',
+	integrations: [mdx(), sitemap()],
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "it", "fr"],
+		routing: {
+			prefixDefaultLocale: true
+		}
+	}
+});
---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE: string = 'INFOGRAPHI';
+export const SITE_DESCRIPTION: string = 'IT, Web & Graphic Design';
+export const TELEPHONE_NUMBER: string = 'tel:+393755848170';
+export const EMAIL_ADDRESS: string = 'mailto:canes.nickels_01@icloud.com';
