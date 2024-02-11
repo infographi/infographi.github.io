7a4426b088215186b7b60df42dedf8991ac5eb34
infographi <purser.profuse0k@icloud.com>
2024-02-11 11:22:48 +0100

    utils i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,31 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+    it: 'Italiano',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.it': 'IT',
+        'nav.branding': 'Web & Branding',
+        'nav.education': 'Education',
+        'nav.about': 'About',
+    },
+    fr: {
+        'nav.home': 'Accueil',
+        'nav.it': 'IT',
+        'nav.branding': 'Web & Branding',
+        'nav.education': 'Formation',
+        'nav.about': 'À propos',
+    },
+    it: {
+        'nav.home': 'Home',
+        'nav.it': 'IT',
+        'nav.branding': 'Web & Branding',
+        'nav.education': 'Educazione',
+        'nav.about': 'Chi Siamo'
+    }
+} as const;
