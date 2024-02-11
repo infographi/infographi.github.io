0d942a9a339d70d86baa372fe8d2daacba71bc5a
infographi <purser.profuse0k@icloud.com>
2024-02-11 11:46:56 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://infographi.github.io',
+	integrations: [sitemap()],
 	i18n: {
 		defaultLocale: "en",
 		locales: ["en", "it", "fr"],
 		routing: {
-			prefixDefaultLocale: false
+			prefixDefaultLocale: true
 		}
 	}
 });
